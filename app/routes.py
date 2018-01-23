1c95a6614303d36929c2d19c6e3c9dcb28c21943
jryoo <jay.ryoo@gmail.com>
2018-01-23 10:52:23 -0800

    add login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 @app.route('/')
 @app.route('/index')
@@ -18,7 +20,13 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # validate_on_submit runs when POST with form data and checks validations
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    # pp.pprint(form.username.errors)
     return render_template('login.html', title='Sign In', form=form)
