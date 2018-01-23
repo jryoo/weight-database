6a9f3146fe74670e7364695d8742f2a7ded23b73
jryoo <jay.ryoo@gmail.com>
2018-01-23 10:15:48 -0800

    add flask-WTF and set configuration from env

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 # add this after app to avoid circular imports
 from app import routes
